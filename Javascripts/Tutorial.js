c198ca0e1d068ae31ca2e270148a9a13f22717f6
Your Name <1234avadhootsmart@gmail.com>
2023-05-18 18:04:24 +0530

    Updating via git

---
+++ Javascripts/Tutorial.js
@@ -0,0 +1,43 @@
+// let myDate= new Date();
+// // console.log(myDate.getTime());
+// // console.log(myDate.getFullYear());
+// // console.log(myDate.getMinutes());
+// // console.log(myDate.getDay
+
+// document.getElementById('click').style.border = 'Blue 2px solid';
+// let elem = document.getElementsByClassName('container');
+// // console.log(elem)
+// elem[0].style.background = "Red";
+
+// // tn = document.getElementsByTagName('div');
+// // console.log(tn)
+// // createdElement = document.createElement('p');
+// // createdElement.innerText = "This is the new element from JS";
+// // tn[0].appendChild(createdElement);
+
+//  a= document.querySelector('.container')
+// console.log(a)
+//  a= document.querySelectorAll('.container')
+// console.log(a)
+
+// b = document.querySelector('.container3')
+// console.log(b)
+// function clicked(){
+//     console.log('The button was clicked');
+// }
+// firstcontainer.addEventListener('click', function(){
+//     console.log("The mouse clicked above the container")
+//     document.querySelectorAll('.container')[0].innerHTML = "<h2> This is an addition via JS</h2>"
+//     html = document.querySelectorAll('.container')[0].innerHTML;
+//     console.log(html)
+// })
+// msg = ()=>{
+//     document.querySelectorAll('.container')[0].innerHTML = "<h2> This is an automated msg via JS</h2>"
+// }
+// setInterval(msg, 2000);
+// localStorage.setItem("Goat", "Messi")
+
+A = `["ate"]`
+A=JSON.parse(A)
+A.push("food")
+console.log(A)
